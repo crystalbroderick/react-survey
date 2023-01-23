b8d3b96472cab74903eb1876a9acff2006cec6c8
crystalbroderick <broderick.crystal@gmail.com>
2023-01-23 04:09:16 -0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+
+function App() {
+
+  return (
+    <Container></Container>
+  )
+}
+
+export default App
